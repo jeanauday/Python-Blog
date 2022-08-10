2bef61982f077d506bb000473ba63da0a7044f53
jeanauday <jean.auday@hotmail.com>
2022-08-10 13:20:12 +0000

    ok

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from flask import flask
+from flask import Flask
 
-app = flask("Hello World")
+app = Flask("Hello")
 
 @app.route("/")
 def hello():
     return "Hello World"
+
+@app.route("/contatos")
+def contatos():
+    return "Jean Auday"
+
+    """<html>
+
+        <head> 
+            <title> Contatos </title>
+        </head>
+
+        <body>
+            <p><h3> Jean Auday</p>
+        </body>
+
+
+    </html>"""
